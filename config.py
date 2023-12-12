b07aaccf1ed194fe01d36b41ed5be384275d26ad
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2023-12-12 18:44:05 +0530

    feat: gender config

--- config.py
+++ config.py
@@ -4,23 +4,21 @@ languages = {
             "hindi": {
                 "translate": "hi_IN",
                 "tts": "hin",
-                "female": 3,
-                "male": 2
             },
         
             "bengali": {
                 "translate": "bn_IN",
                 "tts": "ben",
-                "female": 3,
-                "male": 2
             },
           
             "telugu": {
                 "translate": "te_IN",
                 "tts": "tel",
-                "female": 3,
-                "male": 2
             },
            
+        }
+gender = {
+            "female": 3,
+            "male": 2   
         }
 gpu_devices = [0, 1, 2] 
