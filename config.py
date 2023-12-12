cbad2996627d22847ac697866c23315498e2bda2
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2023-12-12 18:39:59 +0530

    feat: male female config

--- config.py
+++ config.py
@@ -3,17 +3,23 @@ root_dir = "./data"
 languages = {
             "hindi": {
                 "translate": "hi_IN",
-                "tts": "hin"
+                "tts": "hin",
+                "female": 3,
+                "male": 2
             },
         
             "bengali": {
                 "translate": "bn_IN",
-                "tts": "ben"
+                "tts": "ben",
+                "female": 3,
+                "male": 2
             },
           
             "telugu": {
                 "translate": "te_IN",
-                "tts": "tel"
+                "tts": "tel",
+                "female": 3,
+                "male": 2
             },
            
         }
